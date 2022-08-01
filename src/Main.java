bca0398d72c321166c4c8543f285c3098b3a6e87
NadezdaXa <nad3448@yandex.ru>
2022-08-01 22:48:31 +0400

    выполнила дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("пишем дз");
+        System.out.println("делаем коммит и пуш");
+        System.out.println("переходим в гетхаб");
     }
 }
